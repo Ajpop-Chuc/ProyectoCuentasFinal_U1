1edb4e562d15149b03ab46aa361cac3e73e5fdbc
Ajpo Chuc <ajpop_chuc@mesoamericana.edu.gt>
2023-02-27 07:35:59 -0600

    nonse

--- FormBalanceSaldos.Designer.cs
+++ FormBalanceSaldos.Designer.cs
@@ -39,9 +39,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(84, 17);
+            this.label1.Location = new System.Drawing.Point(112, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 23);
+            this.label1.Size = new System.Drawing.Size(281, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Balance De Saldos";
             // 
@@ -52,20 +53,22 @@
             listViewItem1,
             listViewItem2,
             listViewItem3});
-            this.listViewBS.Location = new System.Drawing.Point(109, 59);
+            this.listViewBS.Location = new System.Drawing.Point(132, 71);
+            this.listViewBS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listViewBS.Name = "listViewBS";
-            this.listViewBS.Size = new System.Drawing.Size(162, 227);
+            this.listViewBS.Size = new System.Drawing.Size(248, 278);
             this.listViewBS.TabIndex = 1;
             this.listViewBS.UseCompatibleStateImageBehavior = false;
             // 
             // FormBalanceSaldos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(385, 315);
+            this.ClientSize = new System.Drawing.Size(513, 388);
             this.Controls.Add(this.listViewBS);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormBalanceSaldos";
             this.Text = "FormBalanceSaldos";
             this.Load += new System.EventHandler(this.FormBalanceSaldos_Load);
